5b53b6fc5101e63a12ba60606bb5a9df1f3a6346
Anthoz69 <st.anthoz69@gmail.com>
2019-03-16 12:15:20 +0700

    add new user for seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,61 +12,70 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([ 
-            [   
+        DB::table('users')->insert([
+            [
                 'username' => 'admin',
                 'email' => 'admin@findpho.com',
                 'password' => bcrypt('12345678'),
                 'role_id' => '1',
                 'remember_token' => str_random(10),
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
-            [   
+            [
                 'username' => 'Astudio',
                 'email' => 'Astudio@findpho.com',
                 'password' => bcrypt('12345678'),
                 'role_id' => '2',
                 'remember_token' => str_random(10),
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
-            [   
+            [
                 'username' => 'Bee2019',
                 'email' => 'Bee2019@findpho.com',
                 'password' => bcrypt('12345678'),
                 'role_id' => '3',
                 'remember_token' => str_random(10),
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
-            [   
+            [
                 'username' => 'Aileen',
                 'email' => 'aileen@findpho.com',
                 'password' => bcrypt('12345678'),
                 'role_id' => '3',
                 'remember_token' => str_random(10),
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
-            [   
+            [
                 'username' => 'Renita',
                 'email' => 'renita@findpho.com',
                 'password' => bcrypt('12345678'),
                 'role_id' => '3',
                 'remember_token' => str_random(10),
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
-            [   
+            [
                 'username' => 'MalikaStudio',
                 'email' => 'MalikaStudio@findpho.com',
                 'password' => bcrypt('12345678'),
                 'role_id' => '2',
                 'remember_token' => str_random(10),
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ]
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'username' => 'duodeep',
+                'email' => 'service@duodeep.com',
+                'password' => bcrypt('12345678'),
+                'role_id' => '2',
+                'remember_token' => str_random(10),
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
         ]);
     }
 }
