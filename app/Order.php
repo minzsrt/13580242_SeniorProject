79b2681a94cfb3e94c4f97be7b2456e737396228
Anthoz69 <st.anthoz69@gmail.com>
2019-03-25 10:09:41 +0700

    relate payment

--- app/Order.php
+++ app/Order.php
@@ -9,10 +9,10 @@ class Order extends Model
     protected $guarded = [];
     // protected $fillable = ['price','time_work','date_work','place','detail','transportation_cost','shipping_cost','total','status_order','status_payment'];
 
+    // relation
     public function user()
     {
         return $this->belongsToMany('App\User')->withPivot('id_employer', 'id_photographer');
-
     }
 
     public function employer()
@@ -27,15 +27,16 @@ class Order extends Model
 
     public function category()
     {
-
         return $this->belongsTo('App\Category', 'id_category');
-
     }
 
     public function formattime()
     {
-
         return $this->belongsTo('App\Format_time', 'id_formattime');
+    }
 
+    public function payment()
+    {
+        return $this->hasOne('App\Payment');
     }
 }
